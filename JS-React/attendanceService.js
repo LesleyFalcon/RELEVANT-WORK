110b476f57d36eca044e8b083405db0c3390fa13
Lesley Falcon <70784973+LesleyFalcon@users.noreply.github.com>
2022-06-04 12:16:01 -0700

    Create attendanceService.js

---
+++ JS-React/attendanceService.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import { API_HOST_PREFIX, onGlobalSuccess, onGlobalError } from './serviceHelpers';
+
+const attendanceService = {
+    endpoint: `${API_HOST_PREFIX}/api/attendances`,
+};
+
+const getWorkShops = (pageIndex, pageSize) => {
+    const config = {
+        method: 'GET',
+        url: `${attendanceService.endpoint}/paginate/?pageIndex=${pageIndex}&pageSize=${pageSize}`,
+        withCredentials: true,
+        crossdomain: true,
+        headers: { 'Content-Type': 'application/json' },
+    };
+    return axios(config);
+};
+
+const getSingleWorkShop = (pageIndex, pageSize, sessionId) => {
+    const config = {
+        method: 'GET',
+        url: `${attendanceService.endpoint}/session/${sessionId}?pageIndex=${pageIndex}&pageSize=${pageSize}`,
+        withCredentials: true,
+        crossdomain: true,
+        headers: { 'Content-Type': 'application/json' },
+    };
+    return axios(config).then(onGlobalSuccess).catch(onGlobalError);
+};
+
+const getCurrentWorkshops = (pageIndex, pageSize) => {
+    const config = {
+        method: 'GET',
+        url: `${attendanceService.endpoint}/current?pageIndex=${pageIndex}&pageSize=${pageSize}`,
+        withCredentials: true,
+        crossdomain: true,
+        headers: { 'Content-Type': 'application/json' },
+    };
+    return axios(config);
+};
+
+const update = (payload) => {
+    const config = {
+        method: 'PUT',
+        url: `${attendanceService.endpoint}/${payload.id}`,
+        withCredentials: true,
+        data: payload,
+        crossdomain: true,
+        headers: { 'Content-Type': 'application/json' },
+    };
+    return axios(config)
+        .then(() => {
+            return payload;
+        })
+        .catch(onGlobalError);
+};
+
+export { getWorkShops, getSingleWorkShop, getCurrentWorkshops, update };
